4a0772a4090fe52d6b26937c47e5297d29e479c6
imamseptian <imam.septian11187@gmail.com>
2021-07-17 03:24:34 +0700

    aayaydasd

--- pages/api/youtube/videoinfo.js
+++ pages/api/youtube/videoinfo.js
@@ -38,54 +38,98 @@ export default async function handler(req, res) {
   let hrstart = process.hrtime();
   const videoURL = req.query.videoURL;
   // const info = await ytdl.getInfo(videoURL);
-  try {
-    const info = await ytdl.getInfo(videoURL);
-    let filteredVideo = [];
 
-    for (let i = 0; i < info.formats.length; i++) {
-      if (info.formats[i].container != "mp4") {
-        continue;
-      }
-      // if (info.formats[i].audioCodec == null) {
-      //   continue;
-      // }
-      if (!info.formats[i].hasAudio) {
-        continue;
-      }
-      // filteredVideo.push(info.formats[i]);
-      let fileSize = 0;
-      let xmlreq = new XMLHttpRequest.XMLHttpRequest();
-      xmlreq.open("HEAD", info.formats[i].url, false); // false = Synchronous
+  const info = await ytdl.getInfo(videoURL);
+  let filteredVideo = [];
 
-      xmlreq.send(null); // it will stop here until this xmlreq request is complete
-
-      // when we are here, we already have a response, b/c we used Synchronous XHR
-
-      if (xmlreq.status === 200) {
-        fileSize = xmlreq.getResponseHeader("content-length");
-        info.formats[i].totalSize = formatBytes(fileSize);
-        info.formats[i].fullSize = parseInt(fileSize);
-        filteredVideo.push(info.formats[i]);
-      }
+  for (let i = 0; i < info.formats.length; i++) {
+    if (info.formats[i].container != "mp4") {
+      continue;
     }
+    // if (info.formats[i].audioCodec == null) {
+    //   continue;
+    // }
+    if (!info.formats[i].hasAudio) {
+      continue;
+    }
+    // filteredVideo.push(info.formats[i]);
+    let fileSize = 0;
+    let xmlreq = new XMLHttpRequest.XMLHttpRequest();
+    xmlreq.open("HEAD", info.formats[i].url, false); // false = Synchronous
 
-    info.filteredVideo = filteredVideo;
-    let end = new Date() - start;
-    let hrend = process.hrtime(hrstart);
-    console.info("Execution time: %dms", end);
-    console.info(
-      "Execution time (hr): %dms %dms",
-      hrend[0] / 1000000,
-      hrend[1] / 1000000
-    );
+    xmlreq.send(null); // it will stop here until this xmlreq request is complete
 
-    res.status(200).json(info);
-  } catch (error) {
-    res.status(404).json({
-      message:
-        "Video URL not Valid, please make sure you copy the correct Youtube Video URL",
-    });
+    // when we are here, we already have a response, b/c we used Synchronous XHR
+
+    if (xmlreq.status === 200) {
+      fileSize = xmlreq.getResponseHeader("content-length");
+      info.formats[i].totalSize = formatBytes(fileSize);
+      info.formats[i].fullSize = parseInt(fileSize);
+      filteredVideo.push(info.formats[i]);
+    }
   }
+
+  info.filteredVideo = filteredVideo;
+  let end = new Date() - start;
+  let hrend = process.hrtime(hrstart);
+  console.info("Execution time: %dms", end);
+  console.info(
+    "Execution time (hr): %dms %dms",
+    hrend[0] / 1000000,
+    hrend[1] / 1000000
+  );
+
+  res.status(200).json(info);
+
+  // try {
+  //   const info = await ytdl.getInfo(videoURL);
+  //   let filteredVideo = [];
+
+  //   for (let i = 0; i < info.formats.length; i++) {
+  //     if (info.formats[i].container != "mp4") {
+  //       continue;
+  //     }
+  //     // if (info.formats[i].audioCodec == null) {
+  //     //   continue;
+  //     // }
+  //     if (!info.formats[i].hasAudio) {
+  //       continue;
+  //     }
+  //     // filteredVideo.push(info.formats[i]);
+  //     let fileSize = 0;
+  //     let xmlreq = new XMLHttpRequest.XMLHttpRequest();
+  //     xmlreq.open("HEAD", info.formats[i].url, false); // false = Synchronous
+
+  //     xmlreq.send(null); // it will stop here until this xmlreq request is complete
+
+  //     // when we are here, we already have a response, b/c we used Synchronous XHR
+
+  //     if (xmlreq.status === 200) {
+  //       fileSize = xmlreq.getResponseHeader("content-length");
+  //       info.formats[i].totalSize = formatBytes(fileSize);
+  //       info.formats[i].fullSize = parseInt(fileSize);
+  //       filteredVideo.push(info.formats[i]);
+  //     }
+  //   }
+
+  //   info.filteredVideo = filteredVideo;
+  //   let end = new Date() - start;
+  //   let hrend = process.hrtime(hrstart);
+  //   console.info("Execution time: %dms", end);
+  //   console.info(
+  //     "Execution time (hr): %dms %dms",
+  //     hrend[0] / 1000000,
+  //     hrend[1] / 1000000
+  //   );
+
+  //   res.status(200).json(info);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(404).json({
+  //     message:
+  //       "Video URL not Valid, please make sure you copy the correct Youtube Video URL",
+  //   });
+  // }
 
   // let filteredVideo = [];
 }
