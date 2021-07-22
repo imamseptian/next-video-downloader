297d552e3ab97d574d617ad3c2ec7cb037dd3e19
imamseptian <imam.septian11187@gmail.com>
2021-07-22 18:58:21 +0700

    set https proxy

--- pages/api/tiktok/videoinfo.js
+++ pages/api/tiktok/videoinfo.js
@@ -76,15 +76,15 @@ export default async function sendInfo(req, res) {
   //     });
   //   });
 
-  // const proxyList = new ProxyList();
-  // let data = await proxyList.randomByProtocol("http");
-  // console.log(data);
+  const proxyList = new ProxyList();
+  let data = await proxyList.randomByProtocol("https");
+  console.log(data);
 
   try {
     const videoMeta = await TikTokScraper.getVideoMeta(videoURL, {
       // SET PROXY HERE , IF U WANT USE MORE THAN ONE JUST REPLACE IT WITH ARRAY OF PROXY
-      // proxy: `${data.ip}:${data.port}`,
-      proxy: "45.80.47.205:8080",
+      proxy: `${data.ip}:${data.port}`,
+      // proxy: "45.80.47.205:8080",
       // proxy: "socks5://46.4.96.137:1080",
     });
 
