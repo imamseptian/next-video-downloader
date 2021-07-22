8b4944acad91475c7bda7420ea472c92e25517c0
imamseptian <imam.septian11187@gmail.com>
2021-07-22 20:19:24 +0700

    ayaya

--- pages/api/tiktok/download.js
+++ pages/api/tiktok/download.js
@@ -4,7 +4,7 @@ const TikTokScraper = require("tiktok-scraper");
 export default async function download(req, res) {
   console.log("MY HEADERS");
   console.log(req.headers);
-  if (req.headers.isAndroid) {
+  if (req.headers.isandroid) {
     console.log("IS ANDROID DETECTED");
     console.log(req.headers);
     let url = req.headers.tiktokurl;
